2bdf31599361d60300b88a05be1bc9931b755208
JAHANZAIB <shahzaibnadeem521@gmail.com>
2024-11-09 17:33:15 +0500

    made it responsive

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="hero">
+    <div className="container" id="hero1">
+      <div className="hero" id="hero2">
         <nav>
           <a className="logo">Jahanzaib.</a>
 
-          <div className="nav-links">
+          <div className="nav-links" id="hero3">
             <Link href="#" className="active">Home</Link>
             <Link href="/About">About</Link>
             <Link href="#">Services</Link>
@@ -22,7 +22,7 @@ export default function Home() {
             <h1>Hi, I&apos;m Shahzaib</h1>
             <h4>Frontend Web Developer</h4>
             <p>
-              Hi, I&apos;m Jahanzaib Nadeem, a passionate software developer and tech enthusiast from Karachi, Pakistan. With a keen interest in cybersecurity, software development, and artificial intelligence, I love crafting efficient and innovative solutions that make a difference. My journey in programming has been fueled by a commitment to continuous learning and a drive to tackle challenges head-on. Currently, I specialize in developing robust applications with JavaScript and TypeScript, ensuring a seamless blend of performance and user experience. Insha&apos;Allah, I aspire to contribute meaningfully to the tech world, creating secure and intelligent systems for the future.
+              Hi, I&apos;m Jahanzaib Nadeem, a passionate software developer and tech enthusiast from Karachi, Pakistan. With a keen interest in cybersecurity, software development, and artificial intelligence, I love crafting efficient and innovative solutions that make a difference. My journey in programming has been fueled by a commitment to continuous learning and a drive to tackle challenges head-on. Currently, I specialize in developing robust applications with JavaScript and TypeScript, ensuring a seamless blend of performance and user experience. Insha&apos;Allah.
             </p>
             <a href="/Shahzaib.CV1.pdf.pdf" className="btn" download>Download CV</a>
           </div>
